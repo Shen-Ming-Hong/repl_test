9c690f273be38844dc4a99621208c7380661268d
Rayz <lanskyassassination@gmail.com>
2021-06-13 08:38:15 +0800

    <feat> update

--- 20210606/2.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-Date = datetime.date.today()
-print(Date)  #年月日​
-print(Date.year)  #年​
-print(Date.month)  #月​
-print(Date.day)  #日​
--- 20210606/3.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-#datetime to string​
-
-Date = datetime.date.today()
-
-print(Date.strftime('%d %b %B %Y %y %A %a'))
