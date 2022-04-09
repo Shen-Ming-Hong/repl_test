4fb56483af0c9c1fc5f586b728e4eec80aa35c5b
Rayz <lanskyassassination@gmail.com>
2022-04-09 15:55:17 +0800

    drone test

--- EX3.py
+++
@@ -1,6 +0,0 @@
-students = {'tom': 85, 'mary': 76, 'joe': 58}
-print('學生數 = {}'.format(len(students)))
-print('{} 總分 = {}'.format(
-    str(list(students.values())).replace(',', ' ')[1:-1],
-    sum(students.values())))
-print('平均 = {}'.format(sum(students.values()) / len(students)))
